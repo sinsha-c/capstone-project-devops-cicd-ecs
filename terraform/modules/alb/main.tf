d2695a12ff305568467dce3059f0e407ee63233f
Ubuntu <ubuntu@ip-172-31-2-20.ap-south-1.compute.internal>
2026-09-25 02:01:21 +0000

    alb multiine block tg

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -6,13 +6,22 @@ resource "aws_lb" "this" {
   tags = { Name = "${var.project_name}-alb", Environment = var.environment }
 }
 resource "aws_lb_target_group" "this" {
-  name = "${var.project_name}-tg"
-  port = var.container_port
-  protocol = "HTTP"
+  name        = var.target_group_name
+  port        = var.target_group_port
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
   target_type = "ip"
-  vpc_id = var.vpc_id
-  health_check { enabled = true path = "/" protocol = "HTTP" matcher = "200" interval = 30 timeout = 5 healthy_threshold = 2 unhealthy_threshold = 3 }
-  tags = { Name = "${var.project_name}-tg", Environment = var.environment }
+
+  health_check {
+    enabled             = true
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
 }
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
