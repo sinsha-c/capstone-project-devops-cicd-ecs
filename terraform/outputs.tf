db6626cf8e7a5f63ffda95445474fd2ca7e878c5
Ubuntu <ubuntu@ip-172-31-2-20.ap-south-1.compute.internal>
2026-09-25 02:14:01 +0000

    variables with correct indentation and format

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,39 @@
-output "vpc_id" { value = module.vpc.vpc_id }
-output "public_subnet_ids" { value = module.vpc.public_subnet_ids }
-output "ecr_repository_name" { value = module.ecr.repository_name }
-output "ecr_repository_url" { value = module.ecr.repository_url }
-output "ecs_cluster_name" { value = module.ecs.cluster_name }
-output "ecs_service_name" { value = module.ecs.service_name }
-output "task_definition_arn" { value = module.ecs.task_definition_arn }
-output "alb_dns_name" { value = module.alb.alb_dns_name }
-output "application_url" { value = "http://${module.alb.alb_dns_name}" }
-output "cloudwatch_log_group" { value = module.cloudwatch.log_group_name }
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "ecr_repository_name" {
+  value = module.ecr.repository_name
+}
+
+output "ecr_repository_url" {
+  value = module.ecr.repository_url
+}
+
+output "ecs_cluster_name" {
+  value = module.ecs.cluster_name
+}
+
+output "ecs_service_name" {
+  value = module.ecs.service_name
+}
+
+output "task_definition_arn" {
+  value = module.ecs.task_definition_arn
+}
+
+output "alb_dns_name" {
+  value = module.alb.alb_dns_name
+}
+
+output "application_url" {
+  value = "http://${module.alb.alb_dns_name}"
+}
+
+output "cloudwatch_log_group" {
+  value = module.cloudwatch.log_group_name
+}
