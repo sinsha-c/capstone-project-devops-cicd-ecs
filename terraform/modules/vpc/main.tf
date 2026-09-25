4516520d833dc00da43d2ec61e66d8a2f8acf278
Ubuntu <ubuntu@ip-172-31-2-20.ap-south-1.compute.internal>
2026-09-25 20:47:26 +0000

    filter added to select availability zone

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,5 +1,15 @@
 data "aws_availability_zones" "available" {
   state = "available"
+
+  filter {
+    name   = "zone-type"
+    values = ["availability-zone"]
+  }
+
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
 }
 
 resource "aws_vpc" "this" {
